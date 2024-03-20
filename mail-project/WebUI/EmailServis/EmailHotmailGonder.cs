41a8477df8c7ff269f380cec17926b91b67dbf3c
Oğuzhan Turan <157590022+oguzturan92@users.noreply.github.com>
2024-03-20 23:55:41 +0300

    Update EmailHotmailGonder.cs

--- mail-project/WebUI/EmailServis/EmailHotmailGonder.cs
+++ mail-project/WebUI/EmailServis/EmailHotmailGonder.cs
@@ -9,38 +9,6 @@ namespace WebUI.EmailServis
 {
     public class EmailHotmailGonder : IEmailGonder
     {
-        // Injection
-        private string _host;
-        private int _port;
-        private bool _enableSSL;
-        private string _username;
-        private string _password;
 
-        public EmailHotmailGonder(string host, int port, bool enableSSL, string username, string password)
-        {
-            this._host = host;
-            this._port = port;
-            this._enableSSL = enableSSL;
-            this._username = username;
-            this._password = password;
-        }
-
-        // Metotlar
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
-        {
-            // this. yazanlar, bu sayfada oluşturulmuş olanlar. Diğerleri ise IEmailGonder(yani interface) dosyasından gelenler.
-
-            var client = new SmtpClient(this._host, this._port)
-            {
-                Credentials = new NetworkCredential(_username, _password),
-                EnableSsl = this._enableSSL
-            };
-
-            return client.SendMailAsync(
-                new MailMessage(this._username, email, subject, htmlMessage){
-                    IsBodyHtml = true
-                }
-            );
-        }
     }
 }
