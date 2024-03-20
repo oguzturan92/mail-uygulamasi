09f0f6adcdcf35c808d26251242db27486d9afec
Oğuzhan Turan <157590022+oguzturan92@users.noreply.github.com>
2024-03-20 14:15:55 +0300

    Update ProjeContext.cs

--- mail-project/Data/Concrete/EfCore/ProjeContext.cs
+++ mail-project/Data/Concrete/EfCore/ProjeContext.cs
@@ -18,20 +18,6 @@ namespace Data.Concrete.EfCore
                     // MsSql PC
                 .UseSqlServer(@"Server=.\SQLEXPRESS;Database=mail-project;Integrated Security=True;");
 
-                    // MsSql HOSTİNG
-                // .UseSqlServer("server=77.245.159.27\\MSSQLSERVER2019;database=blog;user=e-ticaret;password=hw45l?44H");
-
-                    // MySqlPC
-                // .UseMySql("server=localhost;port=3306;database=medikal;user=root;password=Ot962962;",
-                //         new MySqlServerVersion(new Version(10, 3, 36)
-                //     )
-                // );
-
-                    // MySqlHOSTING
-                // .UseMySql("server=localhost;port=3306;database=sablon8soft;user=sablon8soft;password=w6j1v&8T8;",
-                //         new MySqlServerVersion(new Version(10, 6, 9)
-                //     )
-                // );
         }
 
         // ENTİTY/CONCRETE içinde oluşturulan tabloların isimlerini, buraya ekliyoruz.
